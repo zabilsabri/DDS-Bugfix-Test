ca9ac6c0caaebd9424f03c8cdc60a81cf97ac215
zabilsabri <zabilsabrimuhammad0@gmail.com>
2025-05-31 12:34:42 +0800

    `feat`: menambahkan factory & seeder untuk data pasien

---
+++ database/factories/PatientFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Patient>
+ */
+class PatientFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'gender' => fake()->randomElement(['male', 'female']),
+            'birthdate' => fake()->date('Y-m-d'),
+        ];
+    }
+}
